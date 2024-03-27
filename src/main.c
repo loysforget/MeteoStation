966ce7e3fd2d8653adcc18645268f2169b8cd67e
loysforget <loysforget@gmail.com>
2024-03-27 11:13:29 +0100

    first wifi attempt

--- src/main.c
+++ src/main.c
@@ -6,15 +6,86 @@
 
 #include "driver/gpio.h"
 #include "dht11.h"
+#include <string.h>
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 #define MAIN_PERIODE 3000
 #define DHT11_GPIO_NUM GPIO_NUM_19
+
+
+
+
+/* The examples use WiFi configuration that you can set via project configuration menu.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
+#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+
+#define AP_SSID "ESP_WIFI"
+#define AP_PASSWORD "12345678"
+#define AP_MAX_CONN 4
+#define AP_CHANNEL 0
+
+static const char *TAG_WIFI = "wifi softAP";
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data){
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG_WIFI, "station "MACSTR" join, AID=%d", MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG_WIFI, "station "MACSTR" leave, AID=%d", MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_softap()
+{
+    esp_netif_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = AP_SSID,
+            .channel = AP_CHANNEL,
+            .password = AP_PASSWORD,
+            .max_connection = AP_MAX_CONN,
+            .ssid_hidden = 0,
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG_WIFI, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+}
+
 
 static const char TAG[] = "main";
 static uint8_t temperature = 0u;
 static uint8_t humidity = 0u; //test
 
-void app_main(void) {
+void taskCapteur(){
     dht11_initialisation(DHT11_GPIO_NUM);
     dht11_mesures data;
     while(1){
@@ -27,5 +98,13 @@ void app_main(void) {
             printf("TEST");
         }
         vTaskDelay(MAIN_PERIODE/portTICK_PERIOD_MS);
-    }  
+    }
 }
+
+void app_main(void) {
+    ESP_LOGI(TAG, "Démarrage du app_main");
+    xTaskCreate(taskCapteur, "taskCapteur", 2046, NULL, 1, NULL);
+    ESP_LOGI(TAG, "Fin du app_main");
+    wifi_init_softap();
+}
+
